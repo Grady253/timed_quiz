01de66ec9767e7f91a90d304110d65bab3b218d4
Grady253 <grady.andre2@gmail.com>
2022-08-15 00:01:12 -0700

    changed a question.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const quizQuestions = [
     {
-        Question: 'What does HTML stand for?',
-        Choice: ['A:Hypertext Markup Language', 'B:Host The Main of the Language', 'C:Host Text Markdown Languange','D:Hypertext Markdown Language'],
-        Answer: 'A:Hypertext Markup Language'
+        Question: 'What is the symbol to comment in javascript?',
+        Choice: ['A: //', 'B: /> ', 'C: ()','D: ||'],
+        Answer: 'A: //'
     },
 
     {
@@ -13,19 +13,19 @@ const quizQuestions = [
 
     {
         Question:'What are Javascript data types?',
-        Choice: ['A:String', 'B:Object', 'C:Numbers', 'D:All of the above'],
+        Choice: ['A: String', 'B: Object', 'C: Numbers', 'D: All of the above'],
         Answer:'D: All of the above'
     },
 
     {
         Question:'What should you always first try to use to declare variables?',
-        Choice: ['A:let', 'B:const', 'c:var'],
+        Choice: ['A:let', 'B: const', 'c: var'],
         Answer:'B:const'
     },
 
     {
         Question:'What does DOM stand for?',
-        Choice: ['A:Document Object Model', 'B: Diverse Opportunity Mode', 'C: Different Object Modes', 'D:Different Operation Methods'],
+        Choice: ['A:Document Object Model', 'B: Diverse Opportunity Mode', 'C: Different Object Modes', 'D: Different Operation Methods'],
         Answer:'A: Document Object Model'
     },
 ]
